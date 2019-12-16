0191a0f576ffac34028aadf0ad629f67ad417b46
samuel mwangi <samuelmwangi729@gmail.com>
2019-12-16 04:35:16 +0300

    news

--- resources/js/components/News.vue
+++ resources/js/components/News.vue
@@ -7,7 +7,7 @@
             <div class="item active" style="background-image:url('img/news/4.jpg');background-size:cover;color:white;background-position:center;height:500px;">
                 <u><h1 class="text-center" style="font-size:30px;opacity:1 !important"> <span style="color:#ff4900">Flora|Digest</span>&nbsp;About Us</h1></u><br><br><br>
                 <h3 class="text-center" style="font-size:30px;font-weight:bold">We are the  leading public service news site in East and Central Africa</h3>
-                <h3 class="text-center" style="font-size:20px;font-weight:bolder;line-height:50px" >We’re impartial and independent, and every day we create distinctive, world-class programmes and content which inform, educate and entertain 
+                <h3 class="text-center" style="font-size:15px;font-weight:bolder;line-height:50px" >We’re impartial and independent, and every day we create distinctive, world-class programmes and content which inform, educate and entertain 
                     millions of people  around the world.</h3><br><br>
                 <h3 class="text-center"><button class="btn" style="border-radius:15px;font-size:20px;background-color:#ff4900 !important;font-weight:bold"><i class="fa fa-angle-double-down"></i></button></h3>
                 <br><br><br><br><br><br><br><br>
@@ -15,20 +15,20 @@
       <!-- writer carousel -->
             <div class="item" style="background-image:url('img/news/8.jpg');background-size:cover;color:black;background-position:center;height:500px">
                 <u><h1 class="text-center" style="font-size:30px;"><span style="color:#ff4900">Flora|Digest</span>&nbsp;Services</h1></u><br><br><br>
-                    <h3 class="text-center" style="font-size:30px;font-weight:bold;background-color:white;">We are the  leading public service news site in East and Central Africa</h3>
-                <h3 class="text-center" style="font-size:30px;font-weight:bold;background-color:white;">Our commercial operations generate income to invest in new programmes and content</h3><br><br>
+                    <h3 class="text-center" style="font-size:15px;font-weight:bold;background-color:white;">We are the  leading public service news site in East and Central Africa</h3>
+                <h3 class="text-center" style="font-size:15px;font-weight:bold;background-color:white;">Our commercial operations generate income to invest in new programmes and content</h3><br><br>
                <h3 class="text-center"><button class="btn" style="border-radius:15px;font-size:20px;background-color:#ff4900 !important;font-weight:bold"><i class="fa fa-info"></i>&nbsp;&nbsp;Order Service</button></h3>
                 <br><br><br><br><br><br><br><br>
             </div>
             <div class="item" style="background-image:url('img/news/5.jpg');background-size:cover;color:black;background-position:center;height:500px">
                 <u><h1 class="text-center" style="font-size:30px;"><span style="color:#ff4900">Flora|Digest</span>&nbsp;<span style="color:white">Reports</span></h1></u><br><br><br>
-                    <h3 class="text-center" style="font-size:30px;font-weight:bold;background-color:white;">Following the launch of our Code of Practice we’ve reviewed and assessed our internal processes to ensure we are able to hold ourselves and others to account</h3>
+                    <h3 class="text-center" style="font-size:15px;font-weight:bold;background-color:white;">Following the launch of our Code of Practice we’ve reviewed and assessed our internal processes to ensure we are able to hold ourselves and others to account</h3>
                 <br><br>
                 <br><br><br><br><br><br><br><br>
             </div>
             <div class="item" style="background-image:url('img/news/7.jpg');background-size:cover;color:black;background-position:center;height:500px">
                 <u><h1 class="text-center" style="font-size:30px;"><span style="color:#ff4900">Flora|Digest</span>&nbsp;Ethics</h1></u><br><br><br>
-                    <h3 class="text-center" style="font-size:20px;font-weight:bold;background-color:white;">The work undertaken through the Code of Practice has 
+                    <h3 class="text-center" style="font-size:15px;font-weight:bold;background-color:white;">The work undertaken through the Code of Practice has 
                         given us a real opportunity to identify areas of excellence within the our Organization and the producers we work with. It has also highlighted opportunities 
                         for improvement and areas for greater investment 
                         to ensure that there is an increase in the wealth of voices, perspectives and stories visible on-screen, on-air and within our productions</h3>
@@ -36,7 +36,7 @@
             </div> 
              <div class="item" style="background-image:url('img/news/1.jpg');background-size:cover;color:black;background-position:center;height:500px">
                 <u><h1 class="text-center" style="font-size:30px;"><span style="color:#ff4900">Flora|Digest</span>&nbsp;Commercials</h1></u><br><br><br>
-                    <h3 class="text-center" style="font-size:30px;font-weight:bold;background-color:white;">The Institutions Board has published a review into the its transparency in the commercial subsidiaries of the BBC.</h3>
+                    <h3 class="text-center" style="font-size:15px;font-weight:bold;background-color:white;">The Institutions Board has published a review into the its transparency in the commercial subsidiaries of the BBC.</h3>
                 <br><br><br><br><br><br><br><br>
             </div>     
           </div>
