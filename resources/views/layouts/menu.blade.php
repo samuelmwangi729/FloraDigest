aecaae29b5776df63ec02766687e5f1622a99e58
samuel mwangi <samuelmwangi729@gmail.com>
2019-12-18 12:31:06 +0300

    Fine UI

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,19 +1,15 @@
 <li class="{{ Request::is('posts*') ? 'active' : '' }}">
-    <a href="{{ route('posts.index') }}"><i class="fa fa-edit"></i><span>Posts</span></a>
+    <a  data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" href="{{ route('posts.index') }}"><i class="fa fa-edit"></i><span>Posts</span></a>
+      <div class="collapse" id="collapseExample">
+        <ul class="list-unstyled" style="color:white;padding-left:30px">
+            <li><a href="{{ route('posts.create') }}" ><i class="fa fa-plus"></i>&nbsp;&nbsp;Create</a></li>
+            <li><a href="#" ><i class="fa fa-eye"></i>&nbsp;&nbsp;View</a></li>
+        </ul>
+      </div>
 </li>
-
 <li class="{{ Request::is('categories*') ? 'active' : '' }}">
     <a href="{{ route('categories.index') }}"><i class="fa fa-edit"></i><span>Categories</span></a>
 </li>
 <li class="{{ Request::is('categories*') ? 'active' : '' }}">
     <a href="{{ route('posts.trashed') }}"><i class="fa fa-trash"></i><span>Recycle Bin</span></a>
-</li>
-<li class="{{ Request::is('posts*') ? 'active' : '' }}">
-    <a  data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" href="{{ route('posts.index') }}"><i class="fa fa-edit"></i><span>Posts</span></a>
-      <div class="collapse" id="collapseExample">
-        <ul class="list-unstyled" style="color:white;padding-left:30px">
-            <li><a href="{{ route('posts.create') }}" ><i class="fa fa-edit"></i>Create</a></li>
-            <li><a href="#" ><i class="fa fa-edit"></i>View</a></li>
-        </ul>
-      </div>
 </li>
