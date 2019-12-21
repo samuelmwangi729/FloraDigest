1e7db9ca5008380d56fea7c6f40c5170d56ef398
samuel mwangi <samuelmwangi729@gmail.com>
2019-12-21 07:13:22 +0300

    New UI

--- resources/views/blog/post/single.blade.php
+++ resources/views/blog/post/single.blade.php
@@ -6,7 +6,7 @@
 </div>
 <div class="container">
     <div class="text-center"><img src="{{ asset($post->image) }}" width="50%" height="50%" class="img-responsive"></div>
-    <div class="row-fluid text-center"><i class="fa fa-user"></i>&nbsp;&nbsp;Posted by Admin&nbsp;&nbsp;&nbsp;&nbsp;
+    <div class="row-fluid"><i class="fa fa-user"></i>&nbsp;&nbsp;Posted by Admin&nbsp;&nbsp;&nbsp;&nbsp;
         <i class="fa fa-clock"></i>&nbsp;&nbsp;{{  $post->created_at->toFormattedDateString() }}&nbsp;&nbsp;&nbsp;&nbsp;
     <i class="fa fa-tags"></i>&nbsp;&nbsp;{{  $post->category->name }}&nbsp;&nbsp;&nbsp;&nbsp;
     </div>
