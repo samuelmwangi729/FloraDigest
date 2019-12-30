fc23ad4762cf15720f2cbc9e3c1ec955c3a4bd77
samuel mwangi <samuelmwangi729@gmail.com>
2019-12-30 13:51:20 +0300

    Paypal

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -46,42 +46,56 @@
            <div class="panel panel-default">
                <div class="panel-body">
                 @if($method=="Paypal")
-                <div id="paypal-button-container" class="text-center text-bold">CheckOut</div>
+                <div id="paypal-button" class="text-center text-bold">CheckOut</div>
                 @endif
                </div>
            </div>
         </div>
     </div>
 </div>
+<script src="https://www.paypalobjects.com/api/checkout.js"></script>
 <script>
-    paypal.Buttons({
-      createOrder: function(data, actions) {
-        return actions.order.create({
-          purchase_units: [{
-            amount: {
-              value: '5000'
-            }
-          }]
-        })
-      },
-      onApprove: function(data, actions) {
-        return actions.redirect();
-        // return actions.order.capture().then(function(details) {
-          
-        //   alert('Transaction completed by ' + details.payer.name.given_name);
-        //   console.log(details);
-        //   // Call your server to save the transaction
-        //   return fetch('/Complete', {
-        //     method: 'post',
-        //     headers: {
-        //       'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //       orderID: data.orderID
-        //     })
-        //   });
-        // });
-      }
-    }).render('#paypal-button-container');
-  </script>
+  paypal.Button.render({
+    // Configure environment
+    env: 'sandbox',
+    client: {
+      sandbox: 'demo_sandbox_client_id',
+      // production: 'demo_production_client_id'
+    },
+    // Customize button (optional)
+    locale: 'en_US',
+    style: {
+      size: 'medium',
+      color: 'gold',
+      shape: 'pill',
+    },
+
+    // Enable Pay Now checkout flow (optional)
+    commit: true,
+
+    // Set up a payment
+    payment: function(data, actions) {
+      return actions.payment.create({
+        redirect_urls:{
+          return_url:'http://localhost:8000/Complete'
+        },
+        transactions: [{
+          amount: {
+            total: '500.01',
+            currency: 'USD'
+          }
+        }]
+      });
+    },
+    // Execute the payment
+    onAuthorize: function(data, actions) {
+      return actions.redirect();
+      // return actions.payment.execute().then(function() {
+      //   // Show a confirmation message to the buyer
+      //   window.alert('Thank you for your purchase!');
+      // });
+    }
+  }, '#paypal-button');
+
+</script>
 @stop
