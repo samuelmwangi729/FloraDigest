ec3976e801ac8f6b144bc37b4d838f432ab6d553
samuel mwangi <samuelmwangi729@gmail.com>
2019-12-30 07:14:49 +0300

    Paypal

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -7,15 +7,31 @@
         <div class="col-sm-8">
            <div class="panel panel-default">
                <div class="panel-body">
-                @php 
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped">
+                        <tr>
+                            <th>product</th>
+                        </tr>
+                        @php 
                 $total=DB::table('carts')->where('user',Auth::user()->email)
                              ->sum('total') + 5000000000 ;
                 @endphp
                  @foreach(App\Cart::where('user',Auth::user()->email)->get()  as $prod)
                  {{-- {{ $prod }} --}}
-                 <img src="{{ asset(App\Models\Products::where('slug',$prod->product_slug)->get()->first()->image1) }}" alt="{{ $prod->slug }}" width="100px" style="padding-bottom:20px"><br>
+                <tr>
+                    <td><img src="{{ asset(App\Models\Products::where('slug',$prod->product_slug)->get()->first()->image1) }}" alt="{{ $prod->slug }}" width="100px" style="padding-bottom:20px"></td>
+                    <td>{{ $prod->product_slug }}</td>
+                    <td>{{ $prod->qty }}</td>
+                    <td>{{ $prod->price }}</td>
+                    <td>{{ $prod->total }}</td>
+                </tr>
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                 <br>
                  @endforeach
+                 {{ $prod }}
                  {{ $total }}
+                    </table>
+                </div>
                </div>
            </div>
         </div>
