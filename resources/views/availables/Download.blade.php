6204d9cb3548bef961801f375597454692b17a94
samuel mwangi <samuelmwangi729@gmail.com>
2020-02-02 05:26:13 +0300

    Share Buttons

--- resources/views/availables/Download.blade.php
+++ resources/views/availables/Download.blade.php
@@ -3,7 +3,7 @@
 <div class="box container">
     <div class="row">
         <div class="col-lg-3 col-md-3 col-sm-3 pull-right" style="font-size:20px;padding-top:80px">
-        if Your Download Does not Start Automatically, Click <a id="download" href="{{asset($link)}}">Here</a> to Manually Download your File
+        Click <a id="download" href="{{asset($link)}}">Here</a> to Manually Download your File
         </div>
         <div class="col-lg-3 col-md-3 col-sm-3 pull-left">
             <i class="fa fa-check-circle pull-right" style="font-size:150px"></i><br>
@@ -13,6 +13,6 @@
 </div>
 @stop
 <script>
-setTimeout(function(){window.location=document.getElementById('download').href}, 10000);
-setTimeout(function(){window.location='/Available/Assignments'}, 6000);
+// setTimeout(function(){window.location=document.getElementById('download').href}, 10000);
+setTimeout(function(){window.location='/Available/Assignments'}, 20000);
 </script>
